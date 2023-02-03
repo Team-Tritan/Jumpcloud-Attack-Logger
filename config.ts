9322aa92baa7eb06d2ed7b19baa7d9980f30189a
Dylan J <dylan@tritan.dev>
2023-02-03 01:49:39 -0500

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,4 +1,5 @@
 "use strict";
 
-export var webhook =
-  "https://discord.com/api/webhooks/1070769746817077299/_LMvtgDYBi5eBW3SyLJgCTRpMDNtOFkyE-5x9eL1Z2ctLlKm2dyfV3DiO3UHH4edz0PD";
+export let webhook = "";
+export let jc_usernane = "";
+export let jc_password = "";
