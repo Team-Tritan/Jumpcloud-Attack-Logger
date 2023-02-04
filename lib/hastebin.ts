7af7339d232f412468bbe7f886cf7a64d8792216
Dylan J <dylan@tritan.dev>
2023-02-04 05:37:31 -0500

    penis

--- lib/hastebin.ts
+++ lib/hastebin.ts
@@ -9,16 +9,12 @@ export default async function postHastebin(
   let data = JSON.stringify(alreadyPosted);
 
   let response = await axios.post("https://bin.tritan.gg/documents", data);
-  let url = "https://bin.tritan.gg" + response.data.key;
+  let url = "https://bin.tritan.gg/" + response.data.key;
 
   let d = new Date();
+  let date = d.toLocaleDateString();
 
-  await axios.post(
-    webhook,
-    JSON.stringify({
-      content: `**${
-        alreadyPosted.length
-      } Failed Attacks on ${d.toDateString()}**\n\nIP Dump: ${url}`,
-    })
-  );
+  await axios.post(webhook, {
+    content: `**${alreadyPosted.length} failed attacks on ${date}**\nAttacker IP Dump: ${url}`,
+  });
 }
