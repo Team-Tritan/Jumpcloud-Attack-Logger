b25e91156cc8d7eac33fb9f0aca23f3017318f07
Dylan James <dylan@tritan.dev>
2023-06-07 23:38:13 -0400

    refactor: function --> err handling

--- utils/clearDump.ts
+++ utils/clearDump.ts
@@ -4,8 +4,13 @@ import { exec } from "child_process";
 
 export default async function clearDumpCache() {
   console.log("Clearing dump directory...");
-  exec("rm ./dump/*.json");
-  console.log("Dump directory cleared.");
+  exec("rm ./dump/*.json", (error, stdout, stderr) => {
+    if (error) {
+      console.error("Error clearing dump directory:", error);
+    } else {
+      console.log("Dump directory cleared.");
+    }
+  });
 
   return;
 }
