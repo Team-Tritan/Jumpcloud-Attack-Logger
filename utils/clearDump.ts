74a73842cb38fc29ccc18eadead67320159320e8
Dylan James <dylan@tritan.dev>
2023-06-07 23:47:05 -0400

    types

--- utils/clearDump.ts
+++ utils/clearDump.ts
@@ -4,7 +4,7 @@ import { exec } from "child_process";
 
 export default async function clearDumpCache() {
   console.log("Clearing dump directory...");
-  exec("rm ./dump/*.json", (error, stdout, stderr) => {
+  exec("rm ./dump/*.json", (error: any, stdout: any, stderr: any) => {
     if (error) {
       console.error("Error clearing dump directory:", error);
     } else {
