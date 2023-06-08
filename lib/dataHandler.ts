b7eb9682e3f4accff4c2da0fce29e08557a463ed
Dylan James <dylan@tritan.dev>
2023-06-07 23:37:06 -0400

    refactored; used specific funcs with better flow for data handling

--- lib/dataHandler.ts
+++ lib/dataHandler.ts
@@ -9,163 +9,140 @@ import abuseReports from "./emailHandler";
 import getIPInfo from "../utils/ipLookup";
 import postHastebin from "../utils/postHastebin";
 
-let directoryPath = path.join(__dirname, "../dump");
-let alreadyPosted: any[] = [];
+const directoryPath = path.join(__dirname, "../dump");
+const alreadyPosted: any[] = [];
 
 export default async function handleDumpedLogs() {
   console.log("Starting log grabber...");
 
-  fs.readdir(directoryPath, function (err, files) {
+  try {
+    const files = await fs.promises.readdir(directoryPath);
     console.log("Reading directory: " + directoryPath);
 
-    if (err) return console.error("Unable to scan: " + err);
-
-    files.forEach(async function (file) {
+    for (const file of files) {
       if (path.extname(file) === ".json") {
-        console.log("Reading file: " + file);
-
-        fs.readFile(
-          path.join(directoryPath, file),
-          "utf-8",
-          async function (error, data) {
-            if (error) return console.error("Unable to read: " + error);
-
-            console.log("Parsing logs in json");
-
-            let jsonData = JSON.parse(data);
-
-            console.log("Filtering for duplicates");
-
-            jsonData = jsonData.filter(
-              (item: any, index: any) =>
-                jsonData.findIndex(
-                  (item2: any) => item2?.src_ip === item?.src_ip
-                ) === index
-            );
-
-            console.log("Found " + jsonData.length + " unique IPs.");
-
-            let i = 0;
-
-            let post = async () => {
-              if (i === jsonData.length) return;
-
-              let item = jsonData[i];
-
-              try {
-                let asn_lookup = await getIPInfo(item.src_ip);
-
-                let payload = [
-                  {
-                    title: `${i + 1} Unique Attacks / ${
-                      jsonData.length
-                    } Total | Ignoring Repeated IPs`,
-                    description: `${item.message}` as string,
-                    color: 0x5865f2,
-                    thumbnail: {
-                      url: "https://64.media.tumblr.com/505f72684d61f8ee355ce5ad5fdd2857/tumblr_nst7fsLmt01rglfeho1_1280.gif",
-                    },
-                    fields: [
-                      {
-                        name: "Timestamp" as string,
-                        value: ("```" +
-                          item.system_timestamp +
-                          "```") as string,
-                        inline: false,
-                      },
-                      {
-                        name: "ARIN ASN/ISP Whois" as string,
-                        value: ("```" +
-                          asn_lookup?.org +
-                          "\n\n" +
-                          asn_lookup?.asn +
-                          "```") as string,
-                        inline: false,
-                      },
-                      {
-                        name: "Attacker IP" as string,
-                        value: ("```" + item.src_ip + "```") as string,
-                        inline: true,
-                      },
-                      {
-                        name: "Attacker Location" as string,
-                        value: ("```" +
-                          `${item?.src_geoip?.region_name || "Unknown City"}, ${
-                            item?.src_geoip?.country_code || "Unknown Country"
-                          }` +
-                          "```") as string,
-                        inline: true,
-                      },
-                      {
-                        name: "Target System" as string,
-                        value: ("```" +
-                          item.system?.hostname +
-                          "```") as string,
-                        inline: true,
-                      },
-                    ],
-                  },
-                ];
-
-                console.log(
-                  `Sending webhook for ${item.src_ip} - ${asn_lookup?.asn} - ${asn_lookup?.org}.`
-                );
-
-                let webhookData = JSON.stringify({
-                  content: null,
-                  embeds: payload,
-                });
-
-                await new Promise((resolve) => setTimeout(resolve, 1000));
-                await axios
-                  .post(config.webhook, webhookData, {
-                    headers: {
-                      "Content-Type": "application/json",
-                    },
-                  })
-                  .then(async () => {
-                    console.log("Webhook delivered successfully");
-                    alreadyPosted.push(item.src_ip);
-                    await abuseReports(item.src_ip);
-                  })
-                  .catch(async (error: any) => {
-                    if (error.response.status === 429) {
-                      return console.log(
-                        "Webhook rate limited, returning",
-                        error.response.data
-                      );
-                    } else
-                      (error: any) => {
-                        console.error(error);
-                      };
-                  });
-
-                i++;
-
-                await post();
-              } catch (e) {
-                console.log(e);
-              }
-            };
-
-            await post();
-
-            await postHastebin(config.webhook, alreadyPosted);
-          }
-        );
+        await processLogFile(file);
       }
+    }
 
-      return;
+    await postHastebin(config.webhook, alreadyPosted);
+  } catch (error) {
+    console.error("Unable to scan directory: " + error);
+  }
+}
+
+async function sendWebhook(payload: any) {
+  try {
+    await axios.post(config.webhook, payload, {
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
+    console.log("Webhook delivered successfully");
+  } catch (error: any) {
+    if (error.response && error.response.status === 429) {
+      console.log("Webhook rate limited, returning", error.response.data);
+    } else {
+      console.error(error);
+    }
+  }
+}
 
-    return;
-  });
+async function processLogFile(file: string) {
+  console.log("Reading file: " + file);
 
-  return;
+  try {
+    const data = await fs.promises.readFile(
+      path.join(directoryPath, file),
+      "utf-8"
+    );
+
+    console.log("Parsing logs in json");
+
+    const jsonData = JSON.parse(data);
+
+    console.log("Filtering for duplicates");
+
+    const uniqueData = jsonData.filter(
+      (item: any, index: any) =>
+        jsonData.findIndex((item2: any) => item2?.src_ip === item?.src_ip) ===
+        index
+    );
+
+    console.log("Found " + uniqueData.length + " unique IPs.");
+
+    for (let i = 0; i < uniqueData.length; i++) {
+      const item = uniqueData[i];
+      const asnLookup = await getIPInfo(item.src_ip);
+
+      const payload = {
+        content: null,
+        embeds: [
+          {
+            title: `${i + 1} Unique Attacks / ${
+              uniqueData.length
+            } Total | Ignoring Repeated IPs`,
+            description: item.message as string,
+            color: 0x5865f2,
+            thumbnail: {
+              url: "https://64.media.tumblr.com/505f72684d61f8ee355ce5ad5fdd2857/tumblr_nst7fsLmt01rglfeho1_1280.gif",
+            },
+            fields: [
+              {
+                name: "Timestamp",
+                value: "```" + item.system_timestamp + "```",
+                inline: false,
+              },
+              {
+                name: "ARIN ASN/ISP Whois",
+                value:
+                  "```" +
+                  (asnLookup?.org || "") +
+                  "\n\n" +
+                  (asnLookup?.asn || "") +
+                  "```",
+                inline: false,
+              },
+              {
+                name: "Attacker IP",
+                value: "```" + item.src_ip + "```",
+                inline: true,
+              },
+              {
+                name: "Attacker Location",
+                value:
+                  "```" +
+                  `${item?.src_geoip?.region_name || "Unknown City"}, ${
+                    item?.src_geoip?.country_code || "Unknown Country"
+                  }` +
+                  "```",
+                inline: true,
+              },
+              {
+                name: "Target System",
+                value: "```" + item.system?.hostname + "```",
+                inline: true,
+              },
+            ],
+          },
+        ],
+      };
+
+      console.log(
+        `Sending webhook for ${item.src_ip} - ${asnLookup?.asn} - ${asnLookup?.org}.`
+      );
+
+      await sendWebhook(payload);
+      alreadyPosted.push(item.src_ip);
+      await abuseReports(item.src_ip);
+    }
+  } catch (error) {
+    console.error("Unable to read file: " + error);
+  }
 }
 
 export async function serveIPList() {
-  let app = express();
+  const app = express();
 
   app.get("/", (req: Request, res: Response) => {
     return res.json({
